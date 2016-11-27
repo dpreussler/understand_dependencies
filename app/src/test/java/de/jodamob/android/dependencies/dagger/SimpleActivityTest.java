6e1926db50060439a5069a828aa4b1a1eb55172e
fixed #172 <preusd@CL02028.local>
2016-11-27 18:41:14 +0200

    add failing test as remainder

---
+++ app/src/test/java/de/jodamob/android/dependencies/dagger/SimpleActivityTest.java
@@ -0,0 +1,13 @@
+package de.jodamob.android.dependencies.dagger;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SimpleActivityTest {
+
+    @Test
+    public void should_track() {
+        fail("TODO");
+    }
+}
